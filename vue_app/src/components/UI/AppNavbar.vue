0ce3e94f1d1c60c644dc4b89ca0602980c696c34
Tobias Quante <t.quante@outlook.com>
2021-11-01 15:46:28 +0100

    feat: add about dropdown section

--- vue_app/src/components/UI/AppNavbar.vue
+++ vue_app/src/components/UI/AppNavbar.vue
@@ -8,18 +8,41 @@
       alt="the q-bit logo of this website"
     />
 
-    <ul class="q-navbar-navigation right">
-      <q-theme-button class="q-navbar-item"></q-theme-button>
-    </ul>
+    <section>
+      <app-dropdown label="About">
+        <section class="q-navbar-about">
+          <h3 class="q-navbar-about__title">
+            <i class="fas fa-info-circle"></i> About this app
+          </h3>
+          <p class="q-navbar-about__paragraph">This simple todo-app is part of the article</p>
+          <blockquote>
+            <a
+              target="_blank"
+              href="https://blog.q-bit.me/a-step-by-step-guide-for-developing-and-deploying-a-vue-js-app-with-docker-part-two/"
+            >A step-by-step guide to develop and deploy Vue apps with docker, part two</a>
+          </blockquote>
+          <p class="q-navbar-about__paragraph">
+            If you've skipped it, you should read the
+            <a
+              target="_blank"
+              href="https://blog.q-bit.me/how-to-develop-and-deploy-a-vue-js-app-with-docker-part-one/"
+            >first article</a> before you jump straight into this one.
+          </p>
+        </section>
+      </app-dropdown>
+      <app-theme-button class="q-navbar-item"></app-theme-button>
+    </section>
   </nav>
 </template>
 
 <script>
-import QThemeButton from "../Theme/AppThemeButton.vue";
+import AppThemeButton from "../Theme/AppThemeButton.vue";
+import AppDropdown from './AppDropdown.vue';
 
 export default {
   components: {
-    QThemeButton,
+    AppThemeButton,
+    AppDropdown,
   },
 };
 </script>
@@ -28,13 +51,9 @@ export default {
 .q-navbar {
   display: flex;
   justify-content: space-between;
-  color: var(--accent-color-primary);
+  color: var(--white-color);
   background-color: var(--accent-color-primary);
   padding: var(--gap-sm);
-}
-
-ul.q-navbar-navigation {
-  align-items: center;
 }
 
 .q-navbar-logo {
@@ -53,13 +72,16 @@ ul.q-navbar-navigation {
   padding: var(--gap-sm) var(--gap-md);
 }
 
-.left {
-  text-align: left;
-  justify-content: flex-start;
+.q-navbar-about {
+  padding: var(--gap-sm) var(--gap-md);
 }
 
-.right {
-  text-align: right;
-  justify-content: flex-end;
+.q-navbar-about__title {
+  font-size: var(--text-size-lg);
+}
+.q-navbar-about__title,
+.q-navbar-about__paragraph {
+  line-height: var(--gap-xl);
+  margin: var(--gap-sm) 0;
 }
 </style>
