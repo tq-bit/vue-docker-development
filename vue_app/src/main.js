f27b6be9434652e8a25a1de093a8fe00a65cf93c
Tobias Quante <t.quante@outlook.com>
2021-11-01 16:40:13 +0100

    feat: register ripple effect

--- vue_app/src/main.js
+++ vue_app/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import vRipple from './directives/vRipple';
 
-createApp(App).mount('#app');
+createApp(App).directive('ripple', vRipple).mount('#app');
